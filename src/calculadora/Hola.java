60d7ca211720fb0a0359d571867c8224b541bcd9
alumno <alumno@DESKTOP-EUBNUM0>
2024-03-15 13:14:23 +0100

    Pues el primero bro

---
+++ src/calculadora/Hola.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+public class Hola {
+	public static void main(String[] args) {
+		for (int i = 0; i < 35; i++) {
+			if (i % 2 == 0)
+				System.out.println("Puede ser par");
+			else
+				System.out.println("No es par");
+		} 
+
+		System.out.println("golA");
+	}
+}
